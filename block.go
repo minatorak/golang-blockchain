2d951e79dc4929f5f8b80f5aed0c3f0f2dc0e495
minatorak <hotmail22866@gmail.com>
2018-11-21 18:51:35 +0700

    remove simple hash data to --> use proof of work generate hash

--- block.go
+++ block.go
@@ -9,10 +9,11 @@ import (
 
 //block valuable information.
 type Block struct {
-	Timestamp     int64
+	Timestamp		int64
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+	Nonce			int
 }
 
 func (b *Block) SetHash() {
@@ -24,8 +25,12 @@ func (b *Block) SetHash() {
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{},0}
+	//block.SetHash() change to use ProofOfWork generate hash
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+	block.Hash = hash[:]
+	block.Nonce = nonce
 	return block
 }
 
