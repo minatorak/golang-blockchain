26e3502151f4e9f7ea52e90eea2509a32455ebcc
minatorak <hotmail22866@gmail.com>
2019-01-09 23:09:56 +0700

    run cli from main

--- cli.go
+++ cli.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
 type CLI struct {
@@ -21,8 +24,14 @@ func (cli *CLI) Run() {
 	switch os.Args[1] {
 	case "addblock":
 		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
 	case "printchain":
 		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
 	default:
 		cli.printUsage()
 		os.Exit(1)
@@ -38,5 +47,45 @@ func (cli *CLI) Run() {
 
 	if printChainCmd.Parsed() {
 		cli.printChain()
+	}
+}
+
+func (cli *CLI) addBlock(data string) {
+	cli.bc.AddBlock(data)
+	fmt.Println("Success!")
+}
+
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
+
+	for {
+		block := bci.Next()
+
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
+	}
+}
+
+
+func (cli *CLI) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  addblock -data BLOCK_DATA - add a block to the blockchain")
+	fmt.Println("  printchain - print all the blocks of the blockchain")
+}
+
+
+
+func (cli *CLI) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		os.Exit(1)
 	}
 }
--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
-	bc := NewBlockchain()
+	//bc := NewBlockchain()
+	//
+	//bc.AddBlock("Send 1 BTC to Minato")
+	//bc.AddBlock("Send 2 BTC to Me")
+	//for _, block := range bc.blocks {
+	//	fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+	//	fmt.Printf("Data: %s\n", block.Data)
+	//	fmt.Printf("Hash: %x\n", block.Hash)
+	//	pow := NewProofOfWork(block)
+	//	fmt.Printf("Pow: %s \n\n",strconv.FormatBool(pow.Validate()))
+	//}
 
-	bc.AddBlock("Send 1 BTC to Minato")
-	bc.AddBlock("Send 2 BTC to Me")
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("Pow: %s \n\n",strconv.FormatBool(pow.Validate()))
-	}
+	bc := NewBlockchain()
+	defer bc.db.Close()
+
+	cli := CLI{bc}
+	cli.Run()
 }
 
